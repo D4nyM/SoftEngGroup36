94878edf83216e94e52d45600eae08898558c03d
Romaine <98655627+RAL0630@users.noreply.github.com>
2022-11-25 10:31:26 -0600

    Update client_prof_manage.php

--- client_prof_manage.php
+++ client_prof_manage.php
@@ -4,14 +4,14 @@
 require_once "config.php";
  
 // Define variables and initialize with empty values
-$name = $address1 = $address2 = $city = $state = $zipcode = "";
+$name = $address1 = $address2 = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     // Check input errors before inserting in database
     // Prepare an insert statement
-    $sql = "UPDATE users SET name = ?, address1= ?, address2= ?, city= ?, state= ?, zipcode= ? WHERE id=id;" ;
+    $sql = "UPDATE users SET name = ?, address1= ?, address2= ?, WHERE id=id;" ;
          
     if($stmt = mysqli_prepare($link, $sql)){
         
@@ -22,9 +22,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $param_name = trim($_POST["name"]);;
 		$param_address1 = trim($_POST["address1"]);;
 		$param_address2 = trim($_POST["address2"]);;
-		$param_city = trim($_POST["city"]);;
-		$param_state = trim($_POST["state"]);;
-		$param_zipcode = trim($_POST["zipcode"]);;
             
         // Attempt to execute the prepared statement
         if(mysqli_stmt_execute($stmt)){
@@ -44,83 +41,62 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 ?>
 <html lang="en">
 <head>
-	<title> Profile Management </title>
+	<title> User Register </title>
 </head>
 <body>
 	<center>
-		<h1>Profile Management</h1> <br>
-		<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+		<h1>User Register</h1> <br>
+		<form action=" " method="post">
 			<label for="name">Full name:</label><br>
 			<input type="text" id="name" name="name" value="" maxlength="50" required><br><br>
   
-			<label for="address1">Address 1:</label><br>
+			<label for="address1">Mailing Address:</label><br>
 			<input type="text" id="address1" name="address1" value="" maxlength="100" required><br><br>
-  
-			<label for="address2">Address 2:</label><br>
-			<input type="text" id="address2" name="address2" value="" maxlength="100" ><br><br>
-  
-			<label for="city">City name:</label><br>
-			<input type="text" id="city" name="city" value="" maxlength="100" required><br><br>
 			
-			<label for="state">State:</label><br>
-			<select name="state" id="state" required>
-				<option value=""> </option>
-				<option value="AL">AL</option>
-				<option value="AK">AK</option>
-				<option value="AR">AR</option>	
-				<option value="AZ">AZ</option>
-				<option value="CA">CA</option>
-				<option value="CO">CO</option>
-				<option value="CT">CT</option>
-				<option value="DC">DC</option>
-				<option value="DE">DE</option>
-				<option value="FL">FL</option>
-				<option value="GA">GA</option>
-				<option value="HI">HI</option>
-				<option value="IA">IA</option>	
-				<option value="ID">ID</option>
-				<option value="IL">IL</option>
-				<option value="IN">IN</option>
-				<option value="KS">KS</option>
-				<option value="KY">KY</option>
-				<option value="LA">LA</option>
-				<option value="MA">MA</option>
-				<option value="MD">MD</option>
-				<option value="ME">ME</option>
-				<option value="MI">MI</option>
-				<option value="MN">MN</option>
-				<option value="MO">MO</option>	
-				<option value="MS">MS</option>
-				<option value="MT">MT</option>
-				<option value="NC">NC</option>	
-				<option value="NE">NE</option>
-				<option value="NH">NH</option>
-				<option value="NJ">NJ</option>
-				<option value="NM">NM</option>			
-				<option value="NV">NV</option>
-				<option value="NY">NY</option>
-				<option value="ND">ND</option>
-				<option value="OH">OH</option>
-				<option value="OK">OK</option>
-				<option value="OR">OR</option>
-				<option value="PA">PA</option>
-				<option value="RI">RI</option>
-				<option value="SC">SC</option>
-				<option value="SD">SD</option>
-				<option value="TN">TN</option>
-				<option value="TX">TX</option>
-				<option value="UT">UT</option>
-				<option value="VT">VT</option>
-				<option value="VA">VA</option>
-				<option value="WA">WA</option>
-				<option value="WI">WI</option>	
-				<option value="WV">WV</option>
-				<option value="WY">WY</option>
-			</select>
-			<br><br>
+			<input type="checkbox" id="myCheck" onclick="myFunction()">  
+			<label for="myCheck">Same as Shipping Address:</label><br><br>
+			
+			<script>
+			function myFunction() {
+				var checkBox = document.getElementById("myCheck");  
+				var textShip = document.getElementById("address1");  
+				var textBil = document.getElementById("address2");  
+				if (checkBox.checked == true){
+					  textBil.value=textShip.value;  
+				} else {
+					  textBil.value="";
+				}
+			}
+			</script>	
   
-			<label for="zipcode">Zipcode:</label><br>
-			<input type="text" id="zipcode" name="zipcode" value="" minlength="5" maxlength="9" required><br><br>
+			<label for="address2">Billing Adress:</label><br>
+			<input type="text" id="address2" name="address2" value="" maxlength="100" ><br><br>
+			
+			<label for="diner">Preferred Diner #:</label><br><br>
+			
+			<label for="points">Earned Points:</label><br><br>
+			
+			<label for="paymentmeth">Preferred Payment Method:</label><br>
+			<script>
+			function onlyOne(checkbox) {
+				var checkboxes = document.getElementsByName('check')
+				checkboxes.forEach((item) => {
+					if (item !== checkbox) item.checked = false
+				})
+			}
+			</script>
+			<table>
+				<tr>
+					<td>Cash</td>
+					<td><input type="checkbox" name="check" onclick="onlyOne(this)"></td>
+					<td>Credit</td>
+					<td><input type="checkbox" name="check" onclick="onlyOne(this)"></td>
+					<td>Check</td>
+					<td><input type="checkbox" name="check" onclick="onlyOne(this)"></td>
+				</tr>
+			</table><br>
+  
+			
 			<input type="submit" value="Submit"> <a href="welcome.php" class="btn btn-warning">Home</a>
 		</form>
 	</center>
