6f27a986617bfa4cc8e8ae8823040d5584e2d8df
Romaine <98655627+RAL0630@users.noreply.github.com>
2022-11-25 09:54:04 -0600

    Add files via upload

---
+++ insert.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  
+<head>
+    <title>Insert Page page</title>
+</head>
+  
+<body>
+    <center>
+        <?php
+  
+        // servername => localhost
+        // username => root
+        // password => empty
+        // database name => login
+        $conn = mysqli_connect("localhost", "root", "", "login");
+          
+        // Check connection
+        if($conn === false){
+            die("ERROR: Could not connect. " 
+                . mysqli_connect_error());
+        }
+          
+        // Taking all 5 values from the form data(input)
+        $username =  $_REQUEST['username'];
+        $password = $_REQUEST['password'];
+        
+          
+        // Performing insert query execution
+        // here our table name is college
+        $sql = "INSERT INTO user_logins  VALUES ('$username', 
+            '$password')";
+          
+        if(mysqli_query($conn, $sql)){
+            echo "<h3>Account Registration Complete" ; 
+			
+        } else{
+            echo "ERROR: Hush! Sorry $sql. " 
+                . mysqli_error($conn);
+        }
+          
+        // Close connection
+        mysqli_close($conn);
+        ?>
+    </center>
+</body>
+  
+</html>
